7cac0678c7722582cdfe4d7e749f77453f54389d
Tyler Huang <tylhuang@umich.edu>
2023-11-21 18:39:49 -0500

    Shows all bills

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,20 +3,20 @@ import { useEffect, useState } from "react";
 const Home = () => {
 	const [ bills, setBills ] = useState(null);
 
-	const fetchBills = async () => {
-		const res = await fetch('http://localhost:4000/api/bills');
-		const data = await res.json();
-		
-		if (res.ok) {
+	useEffect(() => {
+		const fetchBills = async () => {
+		  console.log("fetching");
+		  const res = await fetch('http://localhost:4000/api/bills');
+		  const data = await res.json();
+		  
+		  if (res.ok) {
 			setBills(data);
-			console.log(data);
+		  }
 		}
-	}
-
-	useEffect (() => {
+	  
 		fetchBills();
-	}, []);
-
+	}, []); // empty dependency array to run the effect only on mount
+	
 	if(!bills) {
 		return <div>Loading...</div> 
 	}
@@ -24,8 +24,8 @@ const Home = () => {
 	return (
 		<div className="home">
 			<div className="bills">
-				{ bills & bills.map((bill) => (
-					<p key={bill._id}>{bill.category} {bill.amonut}</p>
+				{ bills && bills.map((bill) => (
+					<p key={bill._id}>{bill.category} - {bill.subcategory}: ${bill.amount}</p>
 				))}
 			</div>
 		</div>
