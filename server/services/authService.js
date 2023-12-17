4db89cc131f6f1d19d10050910d0e8b7fdbe16fa
tyler <tylhuang@umich.edu>
2023-12-16 21:20:21 -0500

    Added some tests for the user endpoints

--- server/services/authService.js
+++ server/services/authService.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcrypt');
 const validator = require('validator');
 const { userModel } = require('../models/userModel');
 
-const validate = (email, password) => {
+const signup = async (email, password) => {
 	if (!email || !password) {
 		throw Error('All fields must be filled in');
 	}
@@ -12,10 +12,7 @@ const validate = (email, password) => {
 	if (!validator.isStrongPassword(password)) {
 		throw Error('Password not strong enough');
 	}
-}
 
-const signup = async (email, password) => {
-	validate(email, password);
 	const exists = await userModel.findOne({ email });
 
 	if (exists) {
