657e4ef49b5dbb122b0071be3c45048184282d6a
tyler <tylhuang@umich.edu>
2023-12-23 00:51:14 -0500

    Fixed logout context to clear bills context

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,11 +1,14 @@
 import { useAuthContext } from './useAuthContext';
+import { useBillsContext } from './useBillsContext';
 
 export const useLogout = () => {
 	const { dispatch } = useAuthContext();
+	const { dispatch: billsDispatch } = useBillsContext();
+
 	const logout = () => {
 		localStorage.removeItem('user');
-
 		dispatch({type: 'LOGOUT'});
+		billsDispatch({type: 'SET_BILLS', payload: null});
 	}
 
 	return { logout };
