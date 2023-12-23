b1ad95fabd45c9510c000dfaa1ee5be5ae47d586
David Lau <60332980+dark1078@users.noreply.github.com>
2023-12-22 21:30:30 -0500

    smart table

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,70 +1,132 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useBillsContext } from "../hooks/useBillsContext";
+import { AgGridReact } from "ag-grid-react";
 
-// components
+import "ag-grid-community/styles/ag-grid.css"; // For structural styles
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
+
 import BillForm from "../components/BillForm";
 import CSVParser from "../components/CSVParser";
-
-// utils
 import { getBills, deleteBill } from "../utils/apiUtils";
 import { displayDate, displayBillAmount } from "../utils/utils";
 
-// TODO:
-// add spinner for loading
 const Home = () => {
-	const { bills, dispatch } = useBillsContext();
-	// TODO: dynamically set the year
-	const year = '2023';
+  const { bills, dispatch } = useBillsContext();
+  const year = "2023";
+  const [gridApi, setGridApi] = useState(null);
 
-	useEffect(() => {
-		try {
-			const fetchBills = async () => {
-				const [ res, data ] = await getBills(year);
-				if (!res.ok) {
-					throw new Error(data.error);
-				}
+  useEffect(() => {
+    const fetchBills = async () => {
+      try {
+        const [res, data] = await getBills(year);
+        if (!res.ok) {
+          throw new Error(data.error);
+        }
+        dispatch({ type: "SET_BILLS", payload: data });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchBills();
+  }, [dispatch, year]);
 
-				dispatch({type: 'SET_BILLS', payload: data});
-			}
-			fetchBills();
-		} catch (error) {
-			console.log(error);
-		}
-	}, [dispatch, year]); // empty dependency array to run the effect only on mount
+  const handleDeleteSelected = async () => {
+    const selectedNodes = gridApi.getSelectedNodes();
+    const selectedIds = selectedNodes.map((node) => node.data._id);
 
-	const handleDelete = async (id) => {
-		try {
-			const [ res, data ] = await deleteBill(year, id);
-			if (!res.ok) {
-				throw new Error(data.error);
-			}
-			
-			dispatch({type: 'DELETE_BILL', payload: data})
-		} catch (error) {
-			console.log(error);
-		}
-	}
-	
-	if(!bills) {
-		return <div>Loading...</div> 
-	}
+    for (const id of selectedIds) {
+      try {
+        const [res] = await deleteBill(year, id);
+        if (!res.ok) {
+          console.log(res);
+          throw new Error(res.error);
+        }
+        dispatch({ type: "DELETE_BILL", payload: id });
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
-	return (
-		<div className="home">
-			{bills && (
-				<div className="bills">
-					{ bills.map((bill) => (
-						<div key={bill._id}>
-							{displayDate(bill.date)} | {bill.category} - {bill.subcategory}: ${displayBillAmount(bill.amount)}
-							<button onClick={handleDelete.bind(this, bill._id)}>Delete</button>
-						</div> 
-					))}
-				</div>
-			)}
-			<BillForm category={'Food'}/>
-			<CSVParser/>
-		</div>
-	)
-}
+    gridApi.deselectAll();
+    const [res, updatedData] = await getBills(year);
+    if (res.ok) {
+      dispatch({ type: "SET_BILLS", payload: updatedData });
+    }
+  };
+
+  const onGridReady = (params) => {
+    setGridApi(params.api);
+  };
+
+  const columns = [
+    {
+      headerCheckboxSelection: true,
+      checkboxSelection: true,
+      sortable: false,
+      width: 50,
+      lockPosition: true,
+      resizable: false,
+      suppressMenu: true,
+      flex: 0,
+    },
+    {
+      headerName: "Date",
+      field: "date",
+      sortable: true,
+      filter: true,
+      valueFormatter: (params) => displayDate(params.value),
+      flex: 1,
+    },
+    {
+      headerName: "Category",
+      field: "category",
+      sortable: true,
+      filter: true,
+      flex: 1,
+    },
+    {
+      headerName: "Subcategory",
+      field: "subcategory",
+      sortable: true,
+      filter: true,
+      flex: 1,
+    },
+    {
+      headerName: "Amount",
+      field: "amount",
+      sortable: true,
+      filter: true,
+      valueFormatter: (params) => displayBillAmount(params.value),
+      flex: 1,
+    },
+  ];
+
+  if (!bills) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="home">
+      <button onClick={handleDeleteSelected} className="delete-button">
+        Delete Selected
+      </button>
+      <div id="myGrid" className="ag-theme-quartz">
+        <AgGridReact
+          rowData={bills}
+          columnDefs={columns}
+          domLayout="autoHeight"
+          pagination={true}
+          paginationPageSize={20}
+          rowSelection="multiple"
+          onGridReady={onGridReady}
+          suppressRowClickSelection
+          suppressCellSelection
+        />
+      </div>
+      <BillForm category={"Food"} />
+      <CSVParser />
+    </div>
+  );
+};
 
 export default Home;
